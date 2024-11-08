20d61cf578f761f2d441cfe55098768d9f695318
diel ramanantsoa <ramanantsoafidel@gmail.com>
2024-11-08 07:50:47 +0300

    chore: Remove DaisyUI dependency from package.json and update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // Inclure tes fichiers SolidJS ici
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclure tes fichiers SolidJS ici
   ],
   theme: {
     extend: {},
   },
-  darkMode: 'class', // Activer le dark mode basé sur la classe
-  plugins: [require('daisyui')],
+  darkMode: "class", // Activer le dark mode basé sur la classe
+  plugins: [],
 };
